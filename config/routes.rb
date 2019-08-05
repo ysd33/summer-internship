2cbf01b94b8939ded3bd8f8cbc765ae79373d151
ysd33 <yusaku112233@gmail.com>
2019-08-05 20:46:59 +0900

    push my repository

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	root "top#index"
-
-	resources :recipes, only: %i(show), module: :recipes
+	resources :recipes, only: %i(show new create edit), module: :recipes do
+		resources :steps, only: %i(new create)
+	end
   
 end
